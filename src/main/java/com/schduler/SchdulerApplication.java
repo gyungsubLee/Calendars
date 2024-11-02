6d2dc2c6069c311e1c520e7f18cf8a0eacc1f936
gyoungsubLee <dltjq2323@gmail.com>
2024-11-02 14:10:26 +0900

    🎉 project init

---
+++ src/main/java/com/schduler/SchdulerApplication.java
@@ -0,0 +1,13 @@
+package com.schduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchdulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchdulerApplication.class, args);
+    }
+
+}
